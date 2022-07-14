4397f115a14e053ac819ee20071e5922c820964e
resultadosonlinecentralab <fifacuenta77@gmail.com>
2022-07-14 16:11:11 -0300

    pushing to github

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const express = require('express')
+
+app.listen(3000, () => { console.log('Server working on http://localhost:3000') })
+app.use((req, res, next) => {
+    res.status(404).send('Not Found')
+})
