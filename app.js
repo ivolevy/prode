5e64adf7f906587d618c6deec71642fc6d5ab326
resultadosonlinecentralab <fifacuenta77@gmail.com>
2022-07-18 16:51:55 -0300

    commit

--- app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-
-app.listen(3000, () => { console.log('Server working on http://localhost:3000') })
-app.use((req, res, next) => {
-    res.status(404).send('Not Found')
-})
